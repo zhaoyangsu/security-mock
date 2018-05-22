21c5ec43f83ded57fc37f60c1946c0618307169c
Shuai Zhu <shuai168zhu@gmail.com>
2018-05-22 15:27:04 -0700

    fill map with splunk json, try new map package

--- mapLeaf.js
+++ mapLeaf.js
@@ -1,59 +1,12 @@
-//basic map config with custom fills, mercator projection
-var map = new Datamap({
- scope: 'usa',
- element: document.getElementById('map'),
- projection: 'mercator',
- height: 700,
- fills: {
-   defaultFill: 'rgb(12,56,118)',
-   lt50: 'rgb(66,42,56)',
-   gt50: 'rgb(14,41,59)',
-   highC: 'rgb(238,219,41)',
-   keepOrange: 'rgb(206,66,27)'
- },
- data: {
-   USA: {fillKey: 'lt50' },
-   RUS: {fillKey: 'lt50' },
-   CAN: {fillKey: 'lt50' },
-   BRA: {fillKey: 'gt50' },
-   ARG: {fillKey: 'gt50'},
-   COL: {fillKey: 'gt50' },
-   AUS: {fillKey: 'gt50' },
-   ZAF: {fillKey: 'gt50' },
-   MAD: {fillKey: 'gt50' }
- },
- arcConfig: {
-   strokeColor: 'rgb(238,219,41)'
- }
-})
+// leaflet and google uses sourced stuff with watermark
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-// Beijing Lat and Long
-//latitude: ‎39.913818,
-//longitude: 116.363625
-
-//sample of the arc plugin
-// map.arc([
-// {
-//  origin: {
-//      latitude: 21.32,
-//      longitude: -84.32
-//  },
-//  destination: {
-//      latitude: 21.32,
-//      longitude: 5.32
-//  }
-// },
-// {
-//    origin: {
-//        latitude: 34.194444,
-//        longitude: -97.67
-//    },
-//    destination: {
-//        latitude: 39.913818,
-//        longitude: 116.363625
-//    }
-// }
-// ], {strokeWidth: 2});
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYXBwMW8iLCJhIjoiY2poaThkbGZnMjJybzM2czJodDgxcWR1YSJ9.rwfA67fCZem6-aDNb8GVkg'
+}).addTo(mymap);
 
 // data taken from splunk maps and processed
 // var dataSet = [];
@@ -64,23 +17,3 @@ var map = new Datamap({
 // console.log(JSON.stringify(dataSet))
 
 var data = [{"latitude":"36.37790","longitude":"-120.95437","128.18.22.151":"2","208.73.210.29":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"36.37790","longitude":"-120.95437","128.18.22.151":"2","208.73.210.29":"1"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"36.37790","longitude":"-120.95437","128.18.22.151":"2","208.73.210.29":"1"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"},{"latitude":"27.99870","longitude":"-82.51560","198.178.124.50":"3"},{"latitude":"32.15080","longitude":"34.88830","62.0.0.115":"4"},{"latitude":"34.05330","longitude":"-118.25490","208.73.210.29":"1"},{"latitude":"37.54020","longitude":"-122.30410","128.18.22.151":"2"},{"latitude":"47.60620","longitude":"-122.33210","174.37.172.101":"1"}];
-
-//bubbles, custom popup on hover template
-var geoPins = [];
-data.forEach(function(geo) {
-  // sometimes there are two ip addresses with counts
-  //console.log(Object.getOwnPropertyNames(geo));
-  obj = {name: Object.getOwnPropertyNames(geo)[2], latitude:geo.latitude, longitude:geo.longitude, radius: 8, fillKey: 'keepOrange'};
-  geoPins.push(obj);
-})
-map.bubbles(geoPins, {
-  popupTemplate: function(geo, data) {
-    return "<div class='hoverinfo'>Threat: " + data.name + "</div>";
-  }
-});
-// [
-//   {name: 'Servers', latitude: 21.32, longitude: 5.32, radius: 10, fillKey: 'keepOrange'},
-//   {name: 'Port Scan', latitude: 34.194444, longitude: -97.67, radius: 13, fillKey: 'highC'},
-//   {name: 'DGA Domain', latitude: 21.32, longitude: -84.32, radius: 8, fillKey: 'highC'},
-//   {name: 'Data Center', latitude: 39.913818, longitude: 116.363625, radius: 17, fillKey: 'keepOrange'},
-//   ]
