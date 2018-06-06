2ae5f90b22f038d3b2edbbcf9e9d98af917638e0
Shuai Zhu <shuai168zhu@gmail.com>
2018-06-06 16:03:43 -0700

    publically host jsgrid.js, add sample data from csv

--- table.js
+++ table.js
@@ -23,35 +23,24 @@ var app = new Vue({
   }
 })
 
-var clients = [
-    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
-];
-
-var countries = [
-    { Name: "", Id: 0 },
-    { Name: "United States", Id: 1 },
-    { Name: "Canada", Id: 2 },
-    { Name: "United Kingdom", Id: 3 }
-];
+var threats = [{ "Category": "南湖云主机", "VDC": "南湖卫生监督所", "Name": "jx-南湖卫生监督所-外交换和大数据交换前置机", "Create_Time": "2017-10-16 15:30:46", "ID": "i-00000536", "Status": "运行中", "Type": "普通虚拟机", "CPU": 1, "Memory": 2, "Disk": 100, "IP": "192.168.31.120,fe80::7959:e63e:b545:7e67;192.168.81.62,fe80::716f:7b2a:6167:4e0c", "Cluster": "JX_Cluster6" },{ "Category": "嘉兴资源池", "VDC": "总工会", "Name": "jx-总工会-数据库服务器2", "Create_Time": "2017-09-15 23:46:10", "ID": "i-000004EF", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 16, "Disk": 1500, "IP": "192.168.30.96,fe80::f9c7:1412:ff28:ebe9", "Cluster": "JX_Cluster6" },{ "Category": "嘉兴资源池", "VDC": "总工会", "Name": "jx-总工会-前置数据库", "Create_Time": "2017-09-15 23:46:03", "ID": "i-000004EE", "Status": "运行中", "Type": "普通虚拟机", "CPU": 4, "Memory": 8, "Disk": 500, "IP": "192.168.30.98,fe80::1452:6653:9f34:29c3", "Cluster": "JX_Cluster6" },{ "Category": "嘉兴资源池", "VDC": "总工会", "Name": "jx-总工会-数据库服务器1", "Create_Time": "2017-09-15 23:45:59", "ID": "i-000004EC", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 16, "Disk": 1500, "IP": "192.168.30.95,fe80::3c2e:ef11:9ef2:8d39", "Cluster": "JX_Cluster6" },{ "Category": "嘉兴资源池", "VDC": "总工会", "Name": "jx-总工会-应用服务器1", "Create_Time": "2017-09-15 23:46:07", "ID": "i-000004EB", "Status": "运行中", "Type": "普通虚拟机", "CPU": 4, "Memory": 16, "Disk": 200, "IP": "192.168.30.97,fe80::f190:8f37:abfc:481b", "Cluster": "JX_Cluster6" },{ "Category": "嘉兴资源池", "VDC": "住房保障局", "Name": "jx-住房保障局-统一配号数据库", "Create_Time": "2017-08-23 14:05:50", "ID": "i-0000047B", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 24, "Disk": 300, "IP": "192.168.162.4,fe80::2a6e:d4ff:fe88:cb51", "Cluster": "JX_Cluster8" },{ "Category": "嘉兴资源池", "VDC": "住房保障局", "Name": "jx-住房保障局-监管分析数据库", "Create_Time": "2017-08-23 14:04:36", "ID": "i-0000047A", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 24, "Disk": 500, "IP": "192.168.162.3,fe80::2a6e:d4ff:fe88:cb50", "Cluster": "JX_Cluster8" },{ "Category": "嘉兴资源池", "VDC": "住房保障局", "Name": "jx-住房保障局-住房保障数据库2", "Create_Time": "2017-08-23 14:02:12", "ID": "i-00000479", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 32, "Disk": 800, "IP": "192.168.162.2,fe80::2a6e:d4ff:fe88:cb4f", "Cluster": "JX_Cluster8" },{ "Category": "嘉兴资源池", "VDC": "住房保障局", "Name": "jx-住房保障局-住房保障数据库1", "Create_Time": "2017-08-23 14:02:12", "ID": "i-00000478", "Status": "运行中", "Type": "普通虚拟机", "CPU": 8, "Memory": 32, "Disk": 800, "IP": "192.168.162.1,fe80::2a6e:d4ff:fe88:cb4e", "Cluster": "JX_Cluster8" }];
 
 $("#jsGrid").jsGrid({
     width: "100%",
-    height: "400px",
+    height: "80%",
     inserting: true,
     editing: true,
-    // sorting: true,
+    sorting: true,
     // paging: true,
-    data: clients,
+    data: threats,
     fields: [
-        { name: "Name", type: "text", width: 150, validate: "required" },
-        { name: "Age", type: "number", width: 50 },
-        { name: "Address", type: "text", width: 200 },
-        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+        { name: "Category", type: "text", width: 70},
+        { name: "VDC", type: "text", width: 100 },
+        { name: "Name", type: "text", width: 200 },
+        { name: "Create_Time", type: "number", width: 100 },
+        { name: "ID", type: "text", width: 100 },
+        { name: "Status", type: "text", width: 50 },
+        { name: "Cluster", type: "text", width: 100 },
         { type: "control" }
     ],
     onItemEditing: function(args) {
