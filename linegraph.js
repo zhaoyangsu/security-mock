dfbc541f2b6b04d6cebf3743a13e1a978ffa0e4a
Shuai Zhu <shuai168zhu@gmail.com>
2018-06-14 14:36:13 -0700

    table title, line graph improvements, etc.

--- linegraph.js
+++ linegraph.js
@@ -1,8 +1,11 @@
 var dom = document.getElementById("lineGraph");
 var graph = echarts.init(dom);
+
 option = {
   title: {
-      text: '折线图堆叠'
+    text: '折线图堆叠',
+    textStyle: {color: '#fff'},
+    left: '7%'
   },
   // linear gradient background color
   backgroundColor: {
@@ -15,21 +18,20 @@ option = {
     }],
   },
   tooltip: {
-      trigger: 'axis'
+    trigger: 'axis'
   },
   legend: {
-      data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+    // data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+    data:['邮件营销','视频广告','直接访问'],
+    textStyle: {
+      color:"#fff"
+    }
   },
   grid: {
       left: '3%',
       right: '4%',
       bottom: '3%',
       containLabel: true
-  },
-  toolbox: {
-      feature: {
-          saveAsImage: {}
-      }
   },
   xAxis: {
     type: 'category',
@@ -46,36 +48,29 @@ option = {
     }
   },
   series: [
-      {
-          name:'邮件营销',
-          type:'line',
-          stack: '总量',
-          data:[120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-          name:'联盟广告',
-          type:'line',
-          stack: '总量',
-          data:[220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-          name:'视频广告',
-          type:'line',
-          stack: '总量',
-          data:[150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-          name:'直接访问',
-          type:'line',
-          stack: '总量',
-          data:[320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-          name:'搜索引擎',
-          type:'line',
-          stack: '总量',
-          data:[820, 932, 901, 934, 1290, 1330, 1320]
-      }
-  ]
+    {
+      name:'邮件营销',
+      type:'line',
+      stack: '总量',
+      data:[120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name:'视频广告',
+      type:'line',
+      stack: '总量',
+      data:[150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name:'直接访问',
+      type:'line',
+      stack: '总量',
+      data:[320, 332, 301, 334, 390, 330, 320]
+    },
+  ],
+  // toolbox: {
+  //   feature: {
+  //     saveAsImage: {}
+  //   }
+  // }
 };
 graph.setOption(option, true);
