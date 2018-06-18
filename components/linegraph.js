eeaa3c3433f6ad5bcaf98617ba65b25405908b89
Shuai Zhu <shuai168zhu@gmail.com>
2018-06-18 14:58:37 -0700

    handle line graph data in yyyy mm dd format

--- components/linegraph.js
+++ components/linegraph.js
@@ -1,11 +1,10 @@
-var dom = document.getElementById("lineGraph");
-var graph = echarts.init(dom);
-
-option = {
+var dom = document.getElementById("linegraph");
+var linegraph = echarts.init(dom);
+linegraph.setOption({
   title: {
+    left: '7%',
     text: '威胁事件趋势',
-    textStyle: {color: '#fff'},
-    left: '7%'
+    textStyle: {color: '#fff'}
   },
   // linear gradient background color
   backgroundColor: {
@@ -17,55 +16,90 @@ option = {
       offset: 1, color: 'rgb(3,22,51)' // color at 100% position
     }],
   },
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    // data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
-    data:['邮件营销','视频广告','直接访问'],
-    textStyle: {
-      color:"#fff"
-    }
-  },
-  grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: ['周一','周二','周三','周四','周五','周六','周日'],
-    axisLabel: {
-      color: '#fff',
-    }
-  },
-  yAxis: {
-    type: 'value',
-    axisLabel: {
-      color: '#fff',
-    }
-  },
-  series: [
-    {
-      name:'邮件营销',
-      type:'line',
-      stack: '总量',
-      data:[120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      name:'视频广告',
-      type:'line',
-      stack: '总量',
-      data:[150, 232, 201, 154, 190, 330, 410]
-    },
-    {
-      name:'直接访问',
-      type:'line',
-      stack: '总量',
-      data:[320, 332, 301, 334, 390, 330, 320]
-    },
-  ],
-};
-graph.setOption(option, true);
+}, true);
+
+// 10.145.89.154:8888/threats/trend/month
+fetch('https://api.myjson.com/bins/1gmhry')
+  .then(response => response.json())
+  .then(jsondata => {
+    formattedArray = [];
+
+    xAxisDate = [];
+    jsondata.forEach(function(obj, index) {
+      a = {value: parseInt(obj.count)};
+
+      var formattedDate = obj.date.substring(4,6) + "/" + obj.date.substring(6,8);
+      xAxisDate.push(formattedDate);
+      formattedArray.push(a)
+    })
+
+    option = {
+      title: {
+        text: '威胁事件趋势',
+        textStyle: {color: '#fff'},
+        left: '7%'
+      },
+      // linear gradient background color
+      backgroundColor: {
+        type: 'linear',
+        x: 0, y: 0, x2: 0, y2: 1,
+        colorStops: [{
+          offset: 0, color: 'rgb(9,31,66)' // color at 0% position
+        }, {
+          offset: 1, color: 'rgb(3,22,51)' // color at 100% position
+        }],
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        // data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+        data:['邮件营销','视频广告','直接访问'],
+        textStyle: {
+          color:"#fff"
+        }
+      },
+      grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        // figure out epoch?
+        data: xAxisDate,
+        axisLabel: {
+          color: '#fff',
+        }
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          color: '#fff',
+        }
+      },
+      series: [
+        {
+          name:'邮件营销',
+          type:'line',
+          stack: '总量',
+          data: formattedArray
+        },
+        // {
+        //   name:'视频广告',
+        //   type:'line',
+        //   stack: '总量',
+        //   data:[150, 232, 201, 154, 190, 330, 410]
+        // },
+        // {
+        //   name:'直接访问',
+        //   type:'line',
+        //   stack: '总量',
+        //   data:[320, 332, 301, 334, 390, 330, 320]
+        // },
+      ],
+    };
+    linegraph.setOption(option, true);
+  })
