f3a894a0108b259d66a337e3872a157f7f19b3e1
Shuai Zhu <shuai168zhu@gmail.com>
2018-06-18 17:34:38 -0700

    circlerose accepts backend call

--- components/circlerose.js
+++ components/circlerose.js
@@ -11,41 +11,50 @@ var data1 = [{value:10, name:'rose1'},
   {value:40, name:'rose8'}];
 var data2 = [{value:100, name:'very high'}]
 
-//https://api.myjson.com/bins/aeuzi
-
-option1 = {
-  title : {
-      text: '威胁等级分布',
-      // subtext: '纯属虚构',
-      // x:'center',
-      textStyle: {color: '#fff'},
-      left: '7%'
-  },
-  backgroundColor: {
-    type: 'linear',
-    x: 0, y: 0, x2: 0, y2: 1,
-    colorStops: [{
-      offset: 0, color: 'rgb(9,31,66)' // color at 0% position
-    }, {
-      offset: 1, color: 'rgb(3,22,51)' // color at 100% position
-    }],
-  },
-  tooltip : {
-      trigger: 'item',
-      formatter: "{a} <br/>{b} : {c} ({d}%)"
-  },
-  calculable : true,
-  series : [
-    {
-      name:'面积模式',
-      type:'pie',
-      radius : [30, 110],
-      roseType : 'area',
-      data: data2
-    }
-  ]
-};
-graph1.setOption(option1, true);
+// 10.145.89.154:5000/threats/area/top4
+fetch('https://api.myjson.com/bins/1ed9e6')
+  .then(response => response.json())
+  .then(jsondata => {
+    formattedArray = [];
+    jsondata.forEach(function(obj, index) {
+      a = {value: obj.pers, name: obj.EventLevel};
+      formattedArray.push(a)
+    })
+    console.log(formattedArray);
+    option1 = {
+      title : {
+          text: '威胁等级分布',
+          // subtext: '纯属虚构',
+          // x:'center',
+          textStyle: {color: '#fff'},
+          left: '7%'
+      },
+      backgroundColor: {
+        type: 'linear',
+        x: 0, y: 0, x2: 0, y2: 1,
+        colorStops: [{
+          offset: 0, color: 'rgb(9,31,66)' // color at 0% position
+        }, {
+          offset: 1, color: 'rgb(3,22,51)' // color at 100% position
+        }],
+      },
+      tooltip : {
+          trigger: 'item',
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      calculable : true,
+      series : [
+        {
+          name:'面积模式',
+          type:'pie',
+          radius : [30, 110],
+          roseType : 'area',
+          data: formattedArray
+        }
+      ]
+    };
+    graph1.setOption(option1, true);
+  })
 
 var dom = document.getElementById("dupecirclerose");
 var graph2 = echarts.init(dom);
@@ -94,4 +103,4 @@ option2 = {
   //   }
   // },
 };
-graph2.setOption(option2, true);
+//graph2.setOption(option2, true);
