e36367740ea6ee80df33a9e76eb68a7c0ee6f76f
Shuai Zhu <shuai168zhu@gmail.com>
2018-06-19 14:32:52 -0700

    duplicate circle rose for bottom left

--- components/circlerose.js
+++ components/circlerose.js
@@ -54,53 +54,40 @@ fetch('https://api.myjson.com/bins/1ed9e6')
       ]
     };
     graph1.setOption(option1, true);
-  })
 
-var dom = document.getElementById("dupecirclerose");
-var graph2 = echarts.init(dom);
-option2 = {
-  title : {
-      text: '威胁等级分布',
-      // subtext: '纯属虚构',
-      // x:'center',
-      textStyle: {color: '#fff'},
-      left: '7%'
-  },
-  backgroundColor: {
-    type: 'linear',
-    x: 0, y: 0, x2: 0, y2: 1,
-    colorStops: [{
-      offset: 0, color: 'rgb(9,31,66)' // color at 0% position
-    }, {
-      offset: 1, color: 'rgb(3,22,51)' // color at 100% position
-    }],
-  },
-  tooltip : {
-      trigger: 'item',
-      formatter: "{a} <br/>{b} : {c} ({d}%)"
-  },
-  calculable : true,
-  series : [
-    {
-      name:'面积模式',
-      type:'pie',
-      radius : [30, 110],
-      roseType : 'area',
-      data: data1
-    }
-  ]
-  // toolbox: {
-  //   show : true,
-  //   feature : {
-  //     mark : {show: true},
-  //     dataView : {show: true, readOnly: false},
-  //     magicType : {
-  //         show: true,
-  //         type: ['pie', 'funnel']
-  //     },
-  //     restore : {show: true},
-  //     saveAsImage : {show: true}
-  //   }
-  // },
-};
-//graph2.setOption(option2, true);
+    var dom = document.getElementById("dupecirclerose");
+    var graph2 = echarts.init(dom);
+    option2 = {
+      title : {
+          text: '威胁等级分布',
+          // subtext: '纯属虚构',
+          // x:'center',
+          textStyle: {color: '#fff'},
+          left: '7%'
+      },
+      backgroundColor: {
+        type: 'linear',
+        x: 0, y: 0, x2: 0, y2: 1,
+        colorStops: [{
+          offset: 0, color: 'rgb(9,31,66)' // color at 0% position
+        }, {
+          offset: 1, color: 'rgb(3,22,51)' // color at 100% position
+        }],
+      },
+      tooltip : {
+          trigger: 'item',
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      calculable : true,
+      series : [
+        {
+          name:'面积模式',
+          type:'pie',
+          radius : [30, 110],
+          roseType : 'area',
+          data: formattedArray
+        }
+      ]
+    };
+    graph2.setOption(option2, true);
+  })
